d2e4c60c21b53aaadb3546696157eb0fd49d5dc7
davedissian <davedissian@hotmail.com>
2021-02-03 19:28:54 -0300

    Subindo projeto

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+export default () =>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            &copy;Dacorporation
+        </a>
---
+++ src/Slogan.js
@@ -0,0 +1,5 @@
+export default () =>
+        <p>
+          Finalmente aprendendo a usar esse desgramado desse React!!
+        </p>
+    
